66defda42e4d54ff6304eea9c7829eafe2541df7
Leah Levenson <LEAH.LEVENSON@baruchmail.cuny.edu>
2026-02-24 00:47:36 +0000

    Updated code in location_dim

--- models/assignment_work/marts/mart_shared/location_dim.sql
+++ models/assignment_work/marts/mart_shared/location_dim.sql
@@ -6,7 +6,7 @@ WITH all_locations AS (
    SELECT DISTINCT
        borough,
        incident_zip AS zip_code
-   FROM {{ ref('stg_311_service_requests_dot') }}
+   FROM {{ ref('stg_311_nyc_dot') }}
    WHERE borough IS NOT NULL
      AND incident_zip IS NOT NULL
 
