29e267575aa07bb98e5cb762c030b9588f8bca4b
Leah Levenson <LEAH.LEVENSON@baruchmail.cuny.edu>
2026-03-02 02:10:08 +0000

    Fixed dim_time

--- models/project_work/marts/mart_shared/dim_time.sql
+++ models/project_work/marts/mart_shared/dim_time.sql
@@ -1,18 +1,24 @@
 WITH all_times AS (
 
-    -- From MVC crashes: construct timestamp safely
+    -- MVC Crashes (crash_date + crash_time → timestamp)
     SELECT DISTINCT
         PARSE_TIMESTAMP(
             '%Y-%m-%d %H:%M',
-            CONCAT(CAST(crash_date AS STRING), ' ', crash_time)
+            CONCAT(
+                CAST(crash_date AS STRING),
+                ' ',
+                crash_time
+            )
         ) AS full_timestamp
     FROM {{ ref('stg_mvc_crashes') }}
-    WHERE crash_date IS NOT NULL AND crash_time IS NOT NULL
+    WHERE crash_date IS NOT NULL
+      AND crash_time IS NOT NULL
 
     UNION DISTINCT
 
-    -- Noise Complaints: already timestamp
-    SELECT DISTINCT created_date AS full_timestamp
+    -- Noise complaints (already timestamp)
+    SELECT DISTINCT
+        created_date AS full_timestamp
     FROM {{ ref('stg_nyc_noise_complaint') }}
     WHERE created_date IS NOT NULL
 ),
@@ -21,12 +27,11 @@ time_dimension AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['full_timestamp']) }} AS time_key,
         full_timestamp AS created_date,
-
         EXTRACT(HOUR FROM full_timestamp) AS hour,
         EXTRACT(MINUTE FROM full_timestamp) AS minute,
         EXTRACT(SECOND FROM full_timestamp) AS second
-
     FROM all_times
 )
 
-SELECT * FROM time_dimension;
+SELECT *
+FROM time_dimension
